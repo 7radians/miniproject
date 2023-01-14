f6d823257576fa21881a44a8d69f1597f6a21f63
Maria Radova <56935802+7radians@users.noreply.github.com>
2023-01-14 22:15:28 +0000

    Updated the visual

--- readfile.py
+++ readfile.py
@@ -13,22 +13,45 @@ dat = NC.Dataset('output_data', 'r', format='NETCDF4')
 ex = dat.variables['Ex'][:,:]
 xpos = dat.variables['x'][:]
 ypos = dat.variables['y'][:]
+xax = dat.variables['x_axis'][:]
+yax = dat.variables['y_axis'][:]
+
+
+# set [-1. 1] axes for the plots and make them look neat
+xaxis = xax[0::10]
+xaxis[-1] = round(xaxis[-1], 1)
+xaxis[0] = round(xaxis[0], 1)
+
+yaxis = yax[0::10]
+yaxis[-1] = round(yaxis[-1], 1)
+yaxis[0] = round(yaxis[0], 1)
+
+xnum = np.linspace(0, len(xax), len(xaxis))
+ynum = np.linspace(0, len(yax), len(yaxis))
+
 
 #a pseudocolour plot
 plt.plot()
-plt.xlabel("x")
-plt.ylabel("y")
+plt.xlabel('x position')
+plt.ylabel('y position')
 plt.imshow(ex,  interpolation='none')
-plt.title("Electric field Ex")
-plt.colorbar()
+plt.title('Electric field Ex')
+plt.colorbar(cmap='jet', label ='Electric field')
+plt.xticks(xnum, xaxis)
+plt.yticks(ynum, yaxis)
+#plt.colorbar.ax.set_ylabel('Electric field', rotation=270)
+
 plt.show()
-    
+
+
 #a scatter plot
 plt.plot()
 plt.scatter(xpos, ypos)
-plt.xlabel("x")
-plt.ylabel("y")
+plt.xlabel("x position")
+plt.ylabel("y position")
 plt.title("Particle position")
+plt.xticks(xaxis)
+plt.yticks(yaxis)
 plt.show()
 
 
