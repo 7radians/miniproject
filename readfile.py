c6ac5040616077533efcecf9794093975462e922
Maria Radova <56935802+7radians@users.noreply.github.com>
2023-01-14 15:19:31 +0000

    Updated the visualisation code
    Will add more changes once everything can be compiled

--- readfile.py
+++ readfile.py
@@ -3,33 +3,32 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import os
+import netCDF4 as NC
 
-# read the variables
-path = os.getcwd()
-path2 = path + '/output.txt'
-print(path2)
-file = netCDF4.Dataset(path2, 'r')
-#edit variable names
-varname1 = file.variables['varname1'][:, :]
-varname2 = file.variables['varname2'][:]
-varname3 = file.variables['varname3'][:]
+#get data
+dat = NC.Dataset('output_data', 'r', format='NETCDF4')
+
+#extract variables
+
+ex = dat.variables['Ex'][:,:]
+xpos = dat.variables['x'][:]
+ypos = dat.variables['y'][:]
 
 #a pseudocolour plot
 plt.plot()
-#plt.figure()
 plt.xlabel("x")
 plt.ylabel("y")
-plt.imshow(varname1,  interpolation='none')
-plt.title("Plot title")
-cax = divider.append_axes("right", size="5%", pad=0.05)
-plt.colorbar(cax=cax)
+plt.imshow(ex,  interpolation='none')
+plt.title("Electric field Ex")
+plt.colorbar()
 plt.show()
     
 #a scatter plot
-plt.scatter(varname2, varname3)
+plt.plot()
+plt.scatter(xpos, ypos)
 plt.xlabel("x")
 plt.ylabel("y")
-plt.title("Plot title")
+plt.title("Particle position")
 plt.show()
+
 
